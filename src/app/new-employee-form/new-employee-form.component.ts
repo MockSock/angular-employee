a3f23c1d8e1f6025d723ba793e212fc172fff681
madden20202 <business.mocksock@gmail.com>
2022-07-08 15:23:46 -0500

    add a warning for user

--- src/app/new-employee-form/new-employee-form.component.ts
+++ src/app/new-employee-form/new-employee-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-new-employee-form',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewEmployeeFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
+
+  newEmployeeForm = this.formBuilder.group({
+    fullName: '',
+    role: '',
+    employmentType: ''
+  })
 
   ngOnInit(): void {
+    this.managerWarning()
   }
 
+  managerWarning() {
+    alert('This form can only be filled out by managers')
+    console.log('An authenticator will be added later that will check the user logged in to authenticate this process')
+  }
 }
