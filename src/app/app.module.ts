3a351ddf4506167cf08119025d58b604e5bb8514
madden20202 <business.mocksock@gmail.com>
2022-07-08 15:18:12 -0500

    add forms and reactive forms module to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
@@ -14,7 +15,9 @@ import { NewEmployeeFormComponent } from './new-employee-form/new-employee-form.
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
