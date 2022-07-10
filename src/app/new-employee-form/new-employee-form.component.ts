f77bc583b190241ded4ce3af320db8fe876384f7
madden20202 <business.mocksock@gmail.com>
2022-07-09 22:12:52 -0500

    finalize code that may work, testing needed

--- src/app/new-employee-form/new-employee-form.component.ts
+++ src/app/new-employee-form/new-employee-form.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FormBuilder, Validators, FormControl} from '@angular/forms';
+import { ApiService } from '../api.service';
+import { Employee } from '../models/employee.model';
 
 @Component({
   selector: 'app-new-employee-form',
@@ -8,12 +10,13 @@ import { FormBuilder, Validators, FormControl} from '@angular/forms';
 })
 export class NewEmployeeFormComponent implements OnInit {
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private api: ApiService) { }
 
   newEmployeeForm = this.formBuilder.group({
-    fullName: new FormControl('', [Validators.required]),
-    role: new FormControl('', [Validators.required]),
-    employmentType: new FormControl('', [Validators.required])
+    employee_id: new FormControl(' ',[Validators.required]),
+    full_name: new FormControl(' ', [Validators.required]),
+    role: new FormControl(' ', [Validators.required]),
+    employment_type: new FormControl(' ', [Validators.required])
   })
 
   ngOnInit(): void {
@@ -21,11 +24,13 @@ export class NewEmployeeFormComponent implements OnInit {
   }
 
   managerWarning() {
-    alert('This form can only be filled out by Big Boss')
+    alert('This form can only be filled out by a manager')
     console.log('An authenticator will be added later that will check the user logged in to authenticate this process')
   }
 
-  createNewEmployee() {
-
+  createNewEmployee(data: any) {
+    this.api.createEmployee(data).subscribe((response: any) =>{
+      console.log(response)
+    })
   }
 }
